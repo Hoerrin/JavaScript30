992d68dbf0d350c78784d575b990981b1670dbfb
Hoerrin <patrykd0604@gmail.com>
2022-09-03 16:49:22 +0200

    Finished day 12

---
+++ Day 12 - Key Sequence Detection (KONAMI CODE) copy/index.js
@@ -0,0 +1,34 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+slideInImages = document.querySelectorAll(".slide-in");
+
+let checkScroll = debounce(() =>
+  slideInImages.forEach((element) => isInViewport(element) && element.classList.add('active'))
+);
+
+window.addEventListener("scroll", checkScroll);
